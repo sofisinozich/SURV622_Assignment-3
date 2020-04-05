205af4ed0715f16c0f1d1d92c084cf64a613b55d
Ben Schneider <benjamin.julius.schneider@gmail.com>
2020-04-04 21:17:58 -0400

    In KNN script, ensure that the outcome variable isn't used as a predictor for the KNN algorithm. Also make sure relevant packages are loaded and fix minor typo with a magrittr pipe.

--- Scripts/model_knn.r
+++ Scripts/model_knn.r
@@ -1,22 +1,33 @@
+library(magrittr)
+library(tidyverse)
+library(caret)
+library(class)
+
 #Load data 
-test_all <- readRDS("test_set.rds")
-train_all <- readRDS("train_set.rds")
+test_all <- readRDS("Data/test_set.rds")
+train_all <- readRDS("Data/train_set.rds")
 
 #Leave in only predictors and outcome (removing coder and ID#)
 test <- 
   test_all %>%
-  select(-c("coder", "status_id")) %>%
+  select(-c("coder", "status_id"))
   
-  train <-
-  train_all %>%
+  train <- 
+    train_all %>%
   select(-c("coder", "status_id"))
 
 #Build model
 #Used 25 as square root of number of obs (rule of thumb value)
 #Also ran 1 for comparison
 
-knn_25 <- knn(train, test, cl = train$code, k =25, l = 0, prob=TRUE)
-knn_1 <- knn(train, test, cl = train$code, k =1, l = 0, prob=TRUE)
+knn_25 <- knn(train %>% select(-code),
+              test %>% select(-code),
+              cl = train$code,
+              k =25, l = 0, prob=TRUE)
+knn_1 <- knn(train %>% select(-code),
+             test %>% select(-code),
+             cl = train$code,
+             k =1, l = 0, prob=TRUE)
 
 
 #Confusion matrix
