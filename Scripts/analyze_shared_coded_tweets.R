5892e15b0c02bc7243b3dbf502dd28f715429a34
Ben Schneider <benjamin.julius.schneider@gmail.com>
2020-04-04 20:51:04 -0400

    Updates to work with custom formatting for one of the recoding files.

--- Scripts/analyze_shared_coded_tweets.R
+++ Scripts/analyze_shared_coded_tweets.R
@@ -16,15 +16,23 @@ library(withr)
       )
   
 # 02 - Import the recoded tweets ----
-  recodings <- list.files('Data/Recodings', full.names = TRUE) %>% 
-    setNames(., str_extract(., "Sofi|Rachael|Ben|Victoria")) %>%
-    map_dfr(read_csv, .id = "coder", col_types = cols(.default = col_character())) %>%
-    # Switch coder names to coder numbers, in order to align with formatting in other data
-    mutate(coder = case_when(coder == "Sofi" ~ 1, coder == "Rachael" ~ 2, coder == "Ben" ~ 3, coder == "Victoria" ~ 4) %>%
-             as.character()) %>%
-    # Remove status ID prefix meant to prevent Excel corrupting the IDs
-    mutate(status_id = str_remove(status_id, "ID_")) %>%
-    select(status_id, coder, code)
+    recodings <- list.files('Data/Recodings', full.names = TRUE) %>% 
+      setNames(., str_extract(., "Sofi|Rachael|Ben|Victoria")) %>%
+      map(read_csv, col_types = cols(.default = col_character())) %>%
+      map_dfr(function(df) {
+        if ("Sentiment" %in% colnames(df)) {
+          select(df, -code) %>% rename(code = Sentiment)
+        } else {
+          df
+        }
+      },
+      .id = "coder") %>%
+      # Switch coder names to coder numbers, in order to align with formatting in other data
+      mutate(coder = case_when(coder == "Sofi" ~ 1, coder == "Rachael" ~ 2, coder == "Ben" ~ 3, coder == "Victoria" ~ 4) %>%
+               as.character()) %>%
+      # Remove status ID prefix meant to prevent Excel corrupting the IDs
+      mutate(status_id = str_remove(status_id, "ID_")) %>%
+      select(status_id, coder, code)
   
 # 03 - Combined with the original codings of tweets done by the original assignees ----
   shared_coded_tweets <- semi_join(x = handcoded_tweets,
@@ -48,10 +56,12 @@ library(withr)
   # Fit a multinomial logistic regression
   # and use a likelihood ratio test
   # to test whether any of the coefficients for coders are nonzero
-  mnom_logit_model <- nnet::multinom(
-    data = shared_coded_tweets,
-    formula = code ~ -1 + coder
-  )
-  
-  lmtest::lrtest(mnom_logit_model)
-  
+    mnom_logit_model <- nnet::multinom(
+      data = shared_coded_tweets,
+      formula = code ~ -1 + coder
+    )
+    
+    lmtest::lrtest(mnom_logit_model)
+    
+  # Estimate standard reliability metrics
+    
