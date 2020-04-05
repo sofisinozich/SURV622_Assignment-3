23dea815a8674df4a9291a729a8398a445b78c81
Ben Schneider <benjamin.julius.schneider@gmail.com>
2020-04-04 21:47:52 -0400

    Clear up code commenting and sectioning.

--- Scripts/train_random_forest.R
+++ Scripts/train_random_forest.R
@@ -6,12 +6,13 @@ library(caret)
 library(randomForest)
 library(ranger)
 
-# Load training/testing sets and process so that the random forest methods from the 'ranger' package can be used
+# 0 - Load training/testing sets and process so that the random forest methods from the 'ranger' package can be used ----
   train_set <- readRDS("Data/train_set.rds") %>%
     # Change outcome variable to a factor
     mutate(code = factor(code, levels = c(1,2,3,4),
                          labels = c("Positive", "Neutral", "Negative", "Irrelevant")))
   
+  # Fix column names which won't play nicely with the caret package
   colnames(train_set)[colnames(train_set) == "\U0001f3c0"] <- "U0001f3c0"
   colnames(train_set)[colnames(train_set) == "\U0001f3c6"] <- "U0001f3c6"
   colnames(train_set)[colnames(train_set) == "\U0001f422"] <- "U0001f422"
@@ -23,11 +24,12 @@ library(ranger)
     mutate(code = factor(code, levels = c(1,2,3,4),
                          labels = c("Positive", "Neutral", "Negative", "Irrelevant")))
   
+  # Fix column names which won't play nicely with the caret package
   colnames(test_set)[colnames(test_set) == "\U0001f3c0"] <- "U0001f3c0"
   colnames(test_set)[colnames(test_set) == "\U0001f3c6"] <- "U0001f3c6"
   colnames(test_set)[colnames(test_set) == "\U0001f422"] <- "U0001f422"
   
-# Establish method for cross-validation tuning
+# 1a - Establish method for cross-validation tuning ----
   cv_folds <- createFolds(train_set[['code']],
                           k = 5,
                           returnTrain = TRUE)
@@ -40,7 +42,7 @@ library(ranger)
                               allowParallel = TRUE,
                               savePredictions = "final")
 
-# Set grid of tuning parameters
+# 1b - Set grid of tuning parameters ----
 
   nvars <- ncol(
     model.matrix(object = code ~ . -1 -coder -status_id,
@@ -57,7 +59,7 @@ library(ranger)
     min.node.size = seq(10, 30, by = 5)
   )
 
-# Use 5-fold cross validation to select tuning parameter values
+# 2 - Use 5-fold cross validation to select tuning parameter values ----
   trained_model <- caret::train(
     form = code ~ . -1 -coder -status_id,
     data = train_set,
@@ -68,8 +70,9 @@ library(ranger)
     num.threads = 3
   )
   
-  # Get performance metrics
+# 3 - Get performance metrics ----
   
+  # Get training set predictions and confusion matrix
   train_set_predictions <- data.frame(
     actual = train_set$code,
     predicted = caret::predict.train(trained_model, newdata = train_set)
@@ -78,28 +81,32 @@ library(ranger)
   train_set_counts <- train_set_predictions %>%
     count(actual, predicted)
     
-  # Overall accuracy
+  # Accuracy on training set
   train_set_accuracy <- divide_by(train_set_counts %>% filter(actual == predicted) %>% pull('n') %>% sum(),
                                   train_set_counts %>% pull('n') %>% sum())
   
-  # Sensitivities
+  print(train_set_accuracy)
+  
+  # Sensitivities on training set
   train_set_counts %>%
     group_by(actual) %>%
     mutate(sensitivity = n/sum(n)) %>%
     filter(actual == predicted) %>%
     ungroup()
   
-# Get test set predictions
+  # Get test set predictions and confusion matrix
   test_set_predictions <- caret::predict.train(trained_model, newdata = test_set)
   
   test_counts <- data.frame(actual = test_set$code, predicted = test_set_predictions) %>%
     count(actual, predicted)
   
-  # Overall accuracy
+  # Accuracy on test set
   test_set_accuracy <- divide_by(test_counts %>% filter(actual == predicted) %>% pull('n') %>% sum(),
                                  test_counts %>% pull('n') %>% sum())
   
-  # Sensitivities
+  print(test_set_accuracy)
+  
+  # Sensitivities on test set
     test_counts %>%
       group_by(actual) %>%
       mutate(pct_of_actual = n/sum(n)) %>%
